baae97d712c0e6ea1c021f69668c9e977668b945
Fernanda Akemi <fernandaakemim@gmail.com>
2023-04-26 15:26:31 -0300

    iniciando curso de React

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Banner from './componentes/Banner/Banner.js';
+import Formulario from './componentes/Formulario';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Banner />
+        <Formulario />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Banner.css';
+
+function Banner() {
+    // JSX
+    return(
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="O banner principal da página do Organo"></img>
+        </header>
+    )
+}
+
+export default Banner;
